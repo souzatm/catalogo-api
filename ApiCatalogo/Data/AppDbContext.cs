26f16f4c36afe6ec29d99dc94eb58828cce80923
Thiago Souza <tsouzatm@outlook.com>
2024-12-11 09:37:17 -0300

    Iniciando projeto

---
+++ ApiCatalogo/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using ApiCatalogo.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiCatalogo.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
+    }
+}
